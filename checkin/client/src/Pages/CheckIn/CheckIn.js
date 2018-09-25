34d2b702812716c614724c8e0e680849f7778e83
Justin Libbee <libbeelibbee@yahoo.com>
2018-09-25 17:12:33 -0600

    adding parts

--- checkin/client/src/Pages/CheckIn/CheckIn.js
+++ checkin/client/src/Pages/CheckIn/CheckIn.js
@@ -1,15 +1,37 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Maps from "../../Components/Map";
+import Nav from "../../Components/Nav"
+import { Container, Row, Col, Button } from "../../Components/Parts"
 
 class CheckIn extends Component {
     render() {
         return (
-            <Maps />
-            // check in button 
-            // send button
-            // log out button? 
+            <div>
+                <Nav />
+                <Container>
+                    <Row>
+                        <Col size="md-12"
+                            className="align-self-center">
+                            <Button
+                                //clickfunction
+                                value="CheckIn"
+                                className="d-block"
+                            />
+                            <Button
+                                //clickfunction
+                                value="Log Out"
+                                className="d-block"
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Maps />
+                    </Row>
+                </Container>
+            </div>
         )
     }
 }
+
 
 export default CheckIn; 
