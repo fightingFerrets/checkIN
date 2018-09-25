b79348726830eaa6bcfc9448a7a73d7c4d74373c
Justin Libbee <libbeelibbee@yahoo.com>
2018-09-24 22:45:19 -0600

    small updates

--- checkin/client/src/App.js
+++ checkin/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./Pages/Home";
+import CheckIn from "./Pages/CheckIn";
 
 
 
@@ -10,6 +11,7 @@ const App = () => (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/checkIn" component={CheckIn} />
       </Switch>
     </div>
   </Router>
---
+++ checkin/client/src/Pages/CheckIn/CheckIn.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Maps from "../../Components/Map";
+
+class CheckIn extends Component {
+    render() {
+        return (
+            <Maps />
+            // check in button 
+            // send button
+            // log out button? 
+        )
+    }
+}
+
+export default CheckIn; 
