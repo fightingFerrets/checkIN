59781de297ca83e81f67f67a3e30c6b7ff9c7eb5
Justin Libbee <libbeelibbee@yahoo.com>
2018-09-25 19:49:03 -0600

    adding API.js and fixing mongoDB info.

--- checkin/models/user.js
+++ checkin/models/user.js
@@ -1,42 +1,16 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-// const bcrypt = require('bcrypt');
-// const SALT_WORK_FACTOR = 10;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const userSchema = new Schema({
-//     name: { type: String, required: true },
-//     userName: { type: String, required: true, index: { unique: true } },
-//     password: { type: String, required: true },
-//     location: {
-//         pin: Schema.Types.Decimal128,
-//         date: { type: date, default: Date.now }
-//     },
-//     sentTo: {
-//         receiver: { type: name },
-//         date: { type: date }
-//     }
-// })
+const userSchema = new Schema({
+    fbUserID: { type: String },
+    mediaURL: { type: String },
+    sentTo: {
+        receiver: { type: String },
+        phoneNum: { type: Number },
+        date: { type: Date }
+    }
+})
 
-// userSchema.pre('save', function (next) {
-//     const user = this;
-//     if (!user.isModified('password')) return next();
+const User = mongoose.model("User", userSchema);
 
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-//         if (err) return next(err);
-//         bcrypt.hash(user.password, salt, function (err, hash) {
-//             if (err) return next(err);
-//             user.password = hash;
-//             next();
-//         });
-//     });
-// });
-
-// userSchema.method.comparePassword = function (candidatePassword, CB) {
-//     bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-//         if (err) return cb(err);
-//         cb(null, isMatch);
-//     });
-// };
-
-
-// module.exports = User;
+module.exports = User;
