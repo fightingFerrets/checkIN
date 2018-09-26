cd9d658ca654f01b53c8ba52217b3bb7796ffe94
Ashley <anoelj86@yahoo.com>
2018-09-25 19:35:25 -0600

    check in function not yet working

--- checkin/client/src/Pages/CheckIn/CheckIn.js
+++ checkin/client/src/Pages/CheckIn/CheckIn.js
@@ -4,7 +4,42 @@ import Nav from "../../Components/Nav"
 import { Container, Row, Col, Button } from "../../Components/Parts"
 
 class CheckIn extends Component {
+
+    state = {
+        phoneNum =[],
+        receiver =[],
+        mediaUrl,
+
+    }
+
+    sendLocation = () => {
+        const MessagingResponse =
+            require("twilio").twiml.MessagingResponse;
+        const client = require('twilio')(accountSid, authToken);
+
+
+        app.post("/sms", (req, res) => {
+
+            phoneNum = req.body.phoneNum;
+            receiver = req.body.receiver;
+            mediaUrl = req.body.mediaUrl;
+            format_number = "+1" + phoneNum;
+
+
+            client.messages
+                .create({ from: '+17024251086', body: 'body', to: format_number, mediaUrl: `http//maps.google.com/?q=${lat},${lng}` })
+                .then(message => console.log(message.sid))
+                .done();
+
+            const twiml = new MessagingResponse();
+            twiml.message("Thanks for signing up!");
+            res.end(twiml.toString());
+        });
+
+    }
     render() {
+
+
         return (
 
             <div>
@@ -17,6 +52,7 @@ class CheckIn extends Component {
                                 //clickfunction
                                 value="CheckIn"
                                 className="d-block"
+                                onClick={this.props.sendLocation}
                             >Send Check In</Button>
                         </Col>
                     </Row>
--- checkin/server.js
+++ checkin/server.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const client = require('twilio')(accountSid, authToken);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,25 +22,3 @@ app.listen(PORT, function () {
 });
 
 ///Twilio
-const MessagingResponse =
-    require("twilio").twiml.MessagingResponse;
-
-sendLocation = () => {
-
-    app.post("/sms", (req, res) => {
-
-        number = req.body.phone_number;
-        first_name = req.body.first_name;
-        format_number = "+1" + number
-
-        client.messages
-            .create({ from: '+17024251086', body: 'body', to: format_number, mediaUrl: "http//maps.google.com/?q=<lat>,<lng>" })
-            .then(message => console.log(message.sid))
-            .done();
-
-        const twiml = new MessagingResponse();
-        twiml.message("Thanks for signing up!");
-        res.end(twiml.toString());
-    });
-
-}
